72357c462743ebc103499fdb200de26fc47699ff
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-05-20 17:22:05 +0500

    fetching api

--- advance_javascript_3/script.js
+++ advance_javascript_3/script.js
@@ -2,14 +2,30 @@
 
 // localStorage.setItem("email", email)
 
-document.getElementById("login-form").addEventListener("submit", function (e) {
-    e.preventDefault() // stop refreshing
-    let email = document.getElementById("email").value
-    let username = document.getElementById("username").value
-    console.log(email);
-    console.log(username);
-    localStorage.setItem("email", email)
-    localStorage.setItem("username", username)
-    alert("Login Successfull")
-    window.location.href = "/home.html"
-})
+// document.getElementById("login-form").addEventListener("submit", function (e) {
+//     e.preventDefault() // stop refreshing
+//     let email = document.getElementById("email").value
+//     let username = document.getElementById("username").value
+//     console.log(email);
+//     console.log(username);
+//     localStorage.setItem("email", email)
+//     localStorage.setItem("username", username)
+//     alert("Login Successfull")
+//     window.location.href = "/home.html"
+// })
+
+
+
+async function apiCalling() {
+    const data = await fetch("https://jsonplaceholder.typicode.com/todos")
+    const response = await data.json()
+    for (let i = 0; i < response.length; i++) {
+        const element = response[i];
+        const data = document.getElementById("data")
+        data.innerHTML += `${(element.title)}${(element.userId)} ${(element.completed)} ${(element.title)}  <br />`;
+
+
+    }
+}
+
+apiCalling()
